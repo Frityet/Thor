603ed51d0c79fae6b1c855fb3501de262839017b
Amrit Bhogal <ambhogal01@gmail.com>
2023-05-05 08:38:00 -0700

    Restart project in C#.
    I love lua but I have an internship coming up using C# so I want to refresh my memory

--- src/mjolnir/actions/init.lua
+++
@@ -1,6 +0,0 @@
----@type { [string] : Action }
-local actions = {
-    create = require("mjolnir.actions.create")
-}
-
-return actions
--- types/thor/config.lua
+++
@@ -1,6 +0,0 @@
-name = "Thunderstore lua type definitions"
-files = {
-    "*.lua"
-}
-
-
