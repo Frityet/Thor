d4aa9bd9714593430d28ba985e87163450c29275
Amrit Bhogal <ambhogal01@gmail.com>
2023-05-07 17:55:07 -0700

    Fixed script

--- generate-models.lua
+++ generate-models.lua
@@ -1,16 +1,19 @@
 local lfs = require("lfs")
-local json = require("cjson.util")
 
-local exec = os.execute
+local l_error = error
+function error(msg, level) return l_error("\x1b[31m"..msg.."\x1b[0m", level) end
 
-exec "openapi-generator generate -v -grust -o ./ -i thunderstore.openai.json"
+print("\x1b[33mGenerating structs...\x1b[0m")
+os.execute("openapi-generator generate -grust -o ./ -i thunderstore.openai.json")
+print("\x1b[32mDone!\x1b[0m")
 
 ---@type string[]
 local nms = {}
 
 for file in lfs.dir("src/models/") --[[@as fun(): string]] do
+    if file == "." or file == ".." or file == "mod.rs" then goto next end
+
     local path = lfs.currentdir().."/src/models/"..file
-    print(path)
 
     ---@type string
     local contents do
@@ -24,20 +27,32 @@ for file in lfs.dir("src/models/") --[[@as fun(): string]] do
     f:write("use serde_derive::{Serialize, Deserialize};\n")
     f:write(contents)
     f:close()
+    print("  \x1b[32mAdded header to \x1b[35m"..file.."\x1b[0m\x1b[0m")
 
     nms[#nms+1] = file:sub(1, -4) -- remove the .rs
+
+    ::next::
 end
 
-local f = assert(io.open("src/models/mod.rs", "r+"))
+print("\x1b[33mGenerating header to \x1b[35mmod.rs\x1b[0m...\x1b[0m")
+local f = assert(io.open("src/models/mod.rs", "w+"))
 
+f:write("#![allow(warnings)]\n")
+
+print("\x1b[33mAdding mods...\x1b[0m")
 for _, name in ipairs(nms) do
     f:write("pub mod "..name..";\n")
+    print("  \x1b[32mAdded \x1b[35m"..name.."\x1b[0m")
 end
+print("\x1b[32mDone!\x1b[0m")
 
+print("\x1b[33mAdding uses...\x1b[0m")
 f:write("pub use crate::models::{\n")
 for _, name in ipairs(nms) do
     f:write("    "..name.."::*,\n")
+    print("  \x1b[32mAdded \x1b[35m"..name.."\x1b[0m")
 end
 f:write("};\n\n")
+print("\x1b[32mDone!\x1b[0m")
 
 f:close()
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,51 +1,98 @@
-use serde::*;
-use serde_derive::{Serialize, Deserialize};
-pub mod api_experimental_community_category_list_200_response;
-pub mod api_experimental_community_list_200_response;
-pub mod api_experimental_package_list_200_response;
-pub mod available_community;
-pub mod community;
-pub mod community_card;
-pub mod community_package_list;
-pub mod completed_part;
-pub mod front_page_content;
-pub mod icon_validator_params;
-pub mod legacy_profile_create_response;
-pub mod manifest_v1_validator_params;
-pub mod markdown_response;
-pub mod ow_login_request_body;
-pub mod ow_login_response_body;
-pub mod package_card;
-pub mod package_category;
-pub mod package_category_experimental;
-pub mod package_dependency;
+#![allow(warnings)]
 pub mod package_detail_view_content;
-pub mod package_experimental;
-pub mod package_listing;
-pub mod package_listing_experimental;
-pub mod package_listing_update_request;
-pub mod package_listing_update_response;
-pub mod package_submission_metadata;
-pub mod package_submission_result;
-pub mod package_version;
-pub mod package_version_experimental;
-pub mod package_wiki;
+pub mod validator_response;
+pub mod api_experimental_package_list_200_response;
+pub mod user_media_finish_upload_params;
+pub mod available_community;
+pub mod package_card;
+pub mod package_dependency;
+pub mod wiki_page_upsert;
+pub mod legacy_profile_create_response;
 pub mod package_wiki_list_response;
+pub mod request_body;
+pub mod package_wiki;
+pub mod package_listing_update_response;
+pub mod package_submission_result;
+pub mod package_version_experimental;
+pub mod response_body;
+pub mod package_experimental;
+pub mod icon_validator_params;
+pub mod package_listing_update_request;
+pub mod community_package_list;
+pub mod upload_part_url;
+pub mod package_submission_metadata;
 pub mod readme_validator_params;
+pub mod package_version;
+pub mod api_experimental_community_list_200_response;
+pub mod wiki;
+pub mod user_media_initiate_upload_response;
+pub mod wiki_page_delete;
+pub mod markdown_response;
+pub mod manifest_v1_validator_params;
+pub mod package_listing_experimental;
 pub mod render_markdown_params;
 pub mod render_markdown_response;
-pub mod request_body;
-pub mod response_body;
-pub mod upload_part_url;
+pub mod package_category_experimental;
+pub mod package_listing;
 pub mod user_media;
-pub mod user_media_finish_upload_params;
-pub mod user_media_initiate_upload_params;
-pub mod user_media_initiate_upload_response;
-pub mod validator_response;
-pub mod wiki;
-pub mod wiki_page;
-pub mod wiki_page_delete;
+pub mod package_category;
+pub mod ow_login_request_body;
+pub mod community_card;
+pub mod completed_part;
 pub mod wiki_page_index;
-pub mod wiki_page_upsert;
+pub mod ow_login_response_body;
+pub mod front_page_content;
+pub mod api_experimental_community_category_list_200_response;
+pub mod wiki_page;
+pub mod community;
+pub mod user_media_initiate_upload_params;
+pub use crate::models::{
+    package_detail_view_content::*,
+    validator_response::*,
+    api_experimental_package_list_200_response::*,
+    user_media_finish_upload_params::*,
+    available_community::*,
+    package_card::*,
+    package_dependency::*,
+    wiki_page_upsert::*,
+    legacy_profile_create_response::*,
+    package_wiki_list_response::*,
+    request_body::*,
+    package_wiki::*,
+    package_listing_update_response::*,
+    package_submission_result::*,
+    package_version_experimental::*,
+    response_body::*,
+    package_experimental::*,
+    icon_validator_params::*,
+    package_listing_update_request::*,
+    community_package_list::*,
+    upload_part_url::*,
+    package_submission_metadata::*,
+    readme_validator_params::*,
+    package_version::*,
+    api_experimental_community_list_200_response::*,
+    wiki::*,
+    user_media_initiate_upload_response::*,
+    wiki_page_delete::*,
+    markdown_response::*,
+    manifest_v1_validator_params::*,
+    package_listing_experimental::*,
+    render_markdown_params::*,
+    render_markdown_response::*,
+    package_category_experimental::*,
+    package_listing::*,
+    user_media::*,
+    package_category::*,
+    ow_login_request_body::*,
+    community_card::*,
+    completed_part::*,
+    wiki_page_index::*,
+    ow_login_response_body::*,
+    front_page_content::*,
+    api_experimental_community_category_list_200_response::*,
+    wiki_page::*,
+    community::*,
+    user_media_initiate_upload_params::*,
+};
 
-pub use crate::models::{ api_experimental_community_category_list_200_response::*, api_experimental_community_list_200_response::*, api_experimental_package_list_200_response::*, available_community::*, community::*, community_card::*, community_package_list::*, completed_part::*, front_page_content::*, icon_validator_params::*, legacy_profile_create_response::*, manifest_v1_validator_params::*, markdown_response::*, ow_login_request_body::*, ow_login_response_body::*, package_card::*, package_category::*, package_category_experimental::*, package_dependency::*, package_detail_view_content::*, package_experimental::*, package_listing::*, package_listing_experimental::*, package_listing_update_request::*, package_listing_update_response::*, package_submission_metadata::*, package_submission_result::*, package_version::*, package_version_experimental::*, package_wiki::*, package_wiki_list_response::*, readme_validator_params::*, render_markdown_params::*, render_markdown_response::*, request_body::*, response_body::*, upload_part_url::*, user_media::*, user_media_finish_upload_params::*, user_media_initiate_upload_params::*, user_media_initiate_upload_response::*, validator_response::*, wiki::*, wiki_page::*, wiki_page_delete::*, wiki_page_index::*, wiki_page_upsert::*,  };
