2aca4dcd6364b7c8a68c3a2e7478b7c70b4e4b05
Amrit Bhogal <ambhogal01@gmail.com>
2023-09-05 19:56:54 -0700

    class conflicts

--- common/src/Common/common.h
+++ common/src/Common/common.h
@@ -1,5 +1,12 @@
 #import "macros.h"
+
+#if defined(__cplusplus)
+#define class class_
+#endif
 #import "ObjFW.h"
+#if defined(__cplusplus)
+#undef class
+#endif
 #import "Version.h"
 #import "InvalidTypeException.h"
 #import "FastPoolAllocator.h"
